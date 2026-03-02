eff34ab2a071e76e5041f5b17d9bf35a8f16e31e
pauled <drpauled@gmail.com>
2026-03-02 11:53:57 -0500

    week 7-1b

--- src/week7polymorphism/TestPoly.java
+++ src/week7polymorphism/TestPoly.java
@@ -2,14 +2,15 @@ package week7polymorphism;
 
 import week6inheritance.HealthPotion;
 import week6inheritance.Location;
+import week6inheritance.Usable;
 import week6inheritance.Weapon;
 
 import java.util.ArrayList;
 
 public class TestPoly {
     public static void main(String[] args) {
-        ArrayList<Location> things=new ArrayList<>();
-        Location l=new HealthPotion(10,1,2);
+        ArrayList<Usable> things=new ArrayList<>();
+        Usable l=new HealthPotion(10,1,2);
         things.add(l);
         l=new Weapon(3,4,5);
         things.add(l);
