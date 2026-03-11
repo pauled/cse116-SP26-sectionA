af49f71ab319a1042d8fab19d167ba5874836661
pauled <drpauled@gmail.com>
2026-03-11 11:52:50 -0400

    week 8-2

--- src/week8testing/CodeToTest.java
+++ src/week8testing/CodeToTest.java
@@ -2,31 +2,64 @@ package week8testing;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class CodeToTest {
     public static int add(int a, int b){
-        return a+b;
+        return a+Math.abs(b);
     }
     @Test
-    public void testAdd(){
-        assertEquals("3==add(1,2)",3,add(1,2));
-        assertEquals("6==add(0,5)",6,add(0,5));
-        assertEquals("3==add(-5,8)",3,add(-5,8));
-        assertTrue(add(1,2)==3);
-        assertTrue(add(0,5)==6);
-        assertTrue(add(-5,8)==3);
-    }
+    public void testAddPositives(){
+        ArrayList<TestAddHelper> tests=new ArrayList<>();
+        tests.add(new TestAddHelper(1,2,3));
+        tests.add(new TestAddHelper(2,5,7));
+        tests.add(new TestAddHelper(6,3,9));
+        tests.add(new TestAddHelper(4,4,8));
 
-    public static void main(String[] args) {
-        System.out.println(add(1,2));
-        System.out.println(add(0,5));
-        System.out.println(add(-5,8));
-        if (add(1,2)==3){
-            System.out.println("passed test add(1,2)==3");
-        } else {
-            System.out.println("failed test add(1,2)==3");
+        for (TestAddHelper test : tests){
+            test.runTest();
         }
     }
+    @Test
+    public void testAddNegatives(){
+        ArrayList<TestAddHelper> tests=new ArrayList<>();
+        tests.add(new TestAddHelper(1,-2,-1));
+        tests.add(new TestAddHelper(2,-5,-3));
+        tests.add(new TestAddHelper(6,-3,3));
+        tests.add(new TestAddHelper(4,-4,0));
+        tests.add(new TestAddHelper(-1,-2,-3));
+        tests.add(new TestAddHelper(-7,-5,-12));
+
+        for (TestAddHelper test : tests){
+            test.runTest();
+        }
+    }
+    @Test
+    public void testAddZeros(){
+        ArrayList<TestAddHelper> tests=new ArrayList<>();
+        tests.add(new TestAddHelper(0,-2,-2));
+        tests.add(new TestAddHelper(0,-5,-5));
+        tests.add(new TestAddHelper(0,3,3));
+        tests.add(new TestAddHelper(0,9,9));
+        tests.add(new TestAddHelper(0,0,0));
+
+        for (TestAddHelper test : tests){
+            test.runTest();
+        }
+    }
+    @Test
+    public void Problems(){
+        double total=0;
+        for (int x=0;x<100;x++){
+            total+=.1;
+        }
+        assertEquals(10,total,.001);
+        String temp="this";
+        assertEquals("this",temp);
+    }
+
+
 }
---
+++ src/week8testing/TestAddHelper.java
@@ -0,0 +1,26 @@
+package week8testing;
+import static org.junit.Assert.assertEquals;
+
+public class TestAddHelper {
+    private int input1;
+    private int input2;
+    private int expected;
+
+    public TestAddHelper(int in1,int in2,int expected){
+        this.input1=in1;
+        this.input2=in2;
+        this.expected=expected;
+    }
+
+    public void runTest(){
+        int actual=CodeToTest.add(this.input1,this.input2);
+        String message=""+this.expected+"==add(";
+        message+=input1+","+input2+")";
+        assertEquals(message,expected,actual);
+
+        actual=CodeToTest.add(this.input2,this.input1);
+        message=""+this.expected+"==add(";
+        message+=input2+","+input1+")";
+        assertEquals(message,expected,actual);
+    }
+}
