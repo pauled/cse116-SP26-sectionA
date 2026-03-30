5da27495a4d847a855385039eb2bf0e61e13c112
pauled <drpauled@gmail.com>
2026-03-30 11:24:07 -0400

    week 11-1a

---
+++ src/week11efficiency/Recursion.java
@@ -0,0 +1,36 @@
+package week11efficiency;
+
+public class Recursion {
+    public static int addToSpotRegRec(int[] vals,int loc){
+        if (loc<=0){
+            return vals[0];
+        } else if (loc>=vals.length){
+            return addToSpotRegRec(vals,loc-1);
+        } else {
+            int temp=addToSpotRegRec(vals,loc-1);
+            return vals[loc]+temp;
+        }
+    }
+    public static int addToSpotTailec(int[] vals,int loc){
+        return addToSpotTailRecHelper(vals,loc,0);
+    }
+    public static int addToSpotTailRecHelper(int[] vals,int loc, int total){
+        if (loc<=0){
+            return vals[0]+total;
+        } else if (loc>=vals.length){
+            return addToSpotTailRecHelper(vals,loc-1,total);
+        } else {
+            return addToSpotTailRecHelper(vals,loc-1,total+vals[loc]);
+        }
+    }
+    public static void main(String[] args) {
+        int [] nums=new int [3];
+        nums[0]=3;
+        nums[1]=6;
+        nums[2]=9;
+        int total1=addToSpotRegRec(nums,3);
+        int total2=addToSpotTailec(nums,3);
+        System.out.println(total1);
+        System.out.println(total2);
+    }
+}
