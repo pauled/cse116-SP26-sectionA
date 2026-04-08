28188e9400b242e2e9a902e9431b4948a1eeee6e
pauled <drpauled@gmail.com>
2026-04-08 11:50:14 -0400

    week 12-2

--- src/week12/Graph.java
+++ src/week12/Graph.java
@@ -1,6 +1,7 @@
 package week12;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Graph<N> {
@@ -18,5 +19,57 @@ public class Graph<N> {
         if (!this.adjacencyList.containsKey(node)){
             this.adjacencyList.put(node,new ArrayList<>());
         }
+    }
+    public String toString(){
+        String out="";
+        for (N node : this.adjacencyList.keySet()){
+            out+=node+": [";
+            for (N to : this.adjacencyList.get(node)){
+                out+=to+" ";
+            }
+            out+="]\n";
+        }
+        return out;
+    }
+    public boolean validPath(ArrayList<N> path){
+        for (int x=0;x<path.size()-1;x++){
+            N from=path.get(x);
+            N to= path.get(x+1);
+            if (!connected(from,to)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean connected(N from,N to){
+        for (N toVal : this.adjacencyList.get(from)){
+            if (toVal.equals(to)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Graph<String> graph=new Graph<>();
+        graph.addEdge("A","B");
+        graph.addEdge("A","C");
+        graph.addEdge("B","A");
+        graph.addEdge("B","D");
+        graph.addEdge("D","E");
+        graph.addEdge("C","E");
+        graph.addEdge("E","F");
+        graph.addEdge("F","G");
+        System.out.println(graph);
+
+        ArrayList<String> path1=new ArrayList<>(Arrays.asList("A","C","E"));
+        System.out.println(graph.validPath(path1));
+
+        ArrayList<String> path2=new ArrayList<>(Arrays.asList("A","C","E","F","G"));
+        System.out.println(graph.validPath(path2));
+        ArrayList<String> path3=new ArrayList<>(Arrays.asList("B","C","E"));
+        System.out.println(graph.validPath(path3));
+        ArrayList<String> path4=new ArrayList<>(Arrays.asList("A","C","E","D"));
+        System.out.println(graph.validPath(path4));
     }
 }
